d9755428b72c53f55af6046d80ede86a656ad010
Joker <nice.way@live.com>
2019-11-02 19:39:49 +0300

    log.SetFlags() added

--- log_tty.go
+++ log_tty.go
@@ -4,6 +4,10 @@
 // '\033[%d;%dm' 			where %d is SGR(0..107)
 // '\033[38;2;%d;%d;%d;m' 	where %d is r;g;b (Cyan = 0;205;205)
 // '\033[38;5;%dm' 			where %d is color index (0..255)
+
+// bash:
+// for COLOR in {1..255}; do echo -en "\e[38;5;${COLOR}m${COLOR} "; done; echo;
+
 package ioterm
 
 import (
@@ -14,45 +18,59 @@ import (
 	c "github.com/Joker/ioterm/color"
 )
 
+const (
+	Ldate         = 1 << iota     // the date in the local time zone: 2009/01/23
+	Ltime                         // the time in the local time zone: 01:23:23
+	Lmicroseconds                 // microsecond resolution: 01:23:23.123123.  assumes Ltime.
+	Llongfile                     // full file name and line number: /a/b/c/d.go:23
+	Lshortfile                    // final file name element and line number: d.go:23. overrides Llongfile
+	LUTC                          // if Ldate or Ltime is set, use UTC rather than the local time zone
+	LstdFlags     = Ldate | Ltime // initial values for the standard logger
+)
 
+var std = log.New(os.Stderr, "", log.Lshortfile)
 
-var std = log.New(os.Stderr, "", log.LstdFlags)
+// log.SetFlags(log.LstdFlags | log.Lshortfile)
+func SetFlags(flag int) {
+	std = log.New(os.Stderr, "", flag)
+}
 
 func Info(v ...interface{}) {
-	std.Output(2, fmt.Sprintf(" %s[INFO]%s  %s\n", c.Green_h, c.Reset, fmt.Sprint(v...)))
+	std.Output(2, fmt.Sprintf(" %s[INFO]%s  %s%s\n", c.Green_h, c.Reset, fmt.Sprint(v...), c.Reset))
 }
 func Note(v ...interface{}) {
-	std.Output(2, fmt.Sprintf(" %s[NOTE]%s  %s\n", c.Blue_h, c.Reset, fmt.Sprint(v...)))
+	std.Output(2, fmt.Sprintf(" %s[NOTE]%s  %s%s\n", c.Blue_h, c.Reset, fmt.Sprint(v...), c.Reset))
 }
 func Warn(v ...interface{}) {
-	std.Output(2, fmt.Sprintf(" %s[WARN]%s  %s\n", c.Yellow_h, c.Reset, fmt.Sprint(v...)))
+	std.Output(2, fmt.Sprintf(" %s[WARN]%s  %s%s\n", c.Yellow_h, c.Reset, fmt.Sprint(v...), c.Reset))
 }
 func Error(v ...interface{}) {
-	std.Output(2, fmt.Sprintf(" %s[ERRO]%s  %s\n", c.Red_h, c.Reset, fmt.Sprint(v...)))
+	std.Output(2, fmt.Sprintf(" %s[ERRO]%s  %s%s\n", c.Red_h, c.Reset, fmt.Sprint(v...), c.Reset))
 }
 func Fatal(v ...interface{}) {
-	std.Output(2, fmt.Sprintf(" %s[FATA]%s  %s\n", c.Red, c.Reset, fmt.Sprint(v...)))
+	std.Output(2, fmt.Sprintf(" %s[FATA]%s  %s%s\n", c.Red, c.Reset, fmt.Sprint(v...), c.Reset))
 }
 func Infof(format string, v ...interface{}) {
-	std.Output(2, fmt.Sprintf(" %s[INFO]%s  %s\n", c.Green_h, c.Reset, fmt.Sprintf(format, v...)))
+	std.Output(2, fmt.Sprintf(" %s[INFO]%s  %s%s\n", c.Green_h, c.Reset, fmt.Sprintf(format, v...), c.Reset))
 }
 func Notef(format string, v ...interface{}) {
-	std.Output(2, fmt.Sprintf(" %s[NOTE]%s  %s\n", c.Blue_h, c.Reset, fmt.Sprintf(format, v...)))
+	std.Output(2, fmt.Sprintf(" %s[NOTE]%s  %s%s\n", c.Blue_h, c.Reset, fmt.Sprintf(format, v...), c.Reset))
 }
 func Warnf(format string, v ...interface{}) {
-	std.Output(2, fmt.Sprintf(" %s[WARN]%s  %s\n", c.Yellow_h, c.Reset, fmt.Sprintf(format, v...)))
+	std.Output(2, fmt.Sprintf(" %s[WARN]%s  %s%s\n", c.Yellow_h, c.Reset, fmt.Sprintf(format, v...), c.Reset))
 }
 func Errorf(format string, v ...interface{}) {
-	std.Output(2, fmt.Sprintf(" %s[ERRO]%s  %s\n", c.Red_h, c.Reset, fmt.Sprintf(format, v...)))
+	std.Output(2, fmt.Sprintf(" %s[ERRO]%s  %s%s\n", c.Red_h, c.Reset, fmt.Sprintf(format, v...), c.Reset))
 }
 func Fatalf(format string, v ...interface{}) {
-	std.Output(2, fmt.Sprintf(" %s[FATA]%s  %s\n", c.Red, c.Reset, fmt.Sprintf(format, v...)))
+	std.Output(2, fmt.Sprintf(" %s[FATA]%s  %s%s\n", c.Red, c.Reset, fmt.Sprintf(format, v...), c.Reset))
 }
 
 // N for 256 colors, where n is color index (0..255)
 func N(n int) string {
 	return fmt.Sprintf("\033[38;5;%dm", n)
 }
+
 // Nb for 256 background colors, where n is color index (0..255)
 func Nb(n int) string {
 	return fmt.Sprintf("\033[48;5;%dm", n)
